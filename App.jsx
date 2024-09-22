7b1fec32fe23602780fd6962d73d04ef456e2131
Rahul Dev Baghel <163354659+rdbaghel@users.noreply.github.com>
2024-09-22 13:30:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+import Cybrom from "./Cybrom"
+import Bhopal from "./Bhopal"
+import Rect from "./Rect"
+
+
+
+const App=()=>{
+    return(
+   
+        < >
+        <div className="rectangle">
+            <div className="rectangle1">
+                <div className="rectangle2">
+                    Cybrom
+                </div>
+            </div>
+        </div>
+{/* 
+      <Bhopal/>
+      <Rect/>
+      <Cybrom/> */}
+      
+
+       </>
+
+
+    )
+}
+export default App;
