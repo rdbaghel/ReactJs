6522e46d700a0a4386fa1e77c7c07359a4967cac
Rahul Dev Baghel <163354659+rdbaghel@users.noreply.github.com>
2024-09-28 14:42:51 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,9 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './r1.css';
 
+{
+
+  }
+  import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render( <App />)
