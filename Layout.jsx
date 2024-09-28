9b6746b0a83551f3036ef3564790973af75369f3
Rahul Dev Baghel <163354659+rdbaghel@users.noreply.github.com>
2024-09-28 14:43:53 +0530

    Add files via upload

---
+++ Layout.jsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from "react-router-dom";
+
+
+
+
+const Layout=()=>{
+
+    return (
+      < >
+      <Link to="home">Home</Link> |
+      <Link to="about">About</Link> |
+      <Link to="contact">Contact</Link> 
+      <hr size="4" color="blue1"/>
+
+
+      <Outlet/>
+      <hr size="4" color="blue1"/>
+      www.mycompany.com
+  
+      </>
+    )
+  }
+  
+  export default Layout;
